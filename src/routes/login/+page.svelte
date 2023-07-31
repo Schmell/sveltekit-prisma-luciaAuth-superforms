c4f605cf19a3cba75018b89fd41966e2802b5183
Schmell <schmell_ya_later@hotmail.com>
2023-07-30 22:35:47 -0400

    more styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
 	import { enhance } from '$app/forms';
 	export let form;
 </script>
 
-<h1>Sign in</h1>
+<h1 class="mb-0">Login</h1>
+<p>Don't have an account? <a href="/register">Register here</a></p>
 {#if form?.message}
 	<p class="text-error">{form.message}</p>
 {/if}
-<div class="py-4">
+<div class="pb-4">
 	<form method="post" use:enhance>
 		<label class="label" for="username">Username</label>
 		<input class="input input-bordered w-full max-w-md" name="username" id="username" /><br />
@@ -18,11 +20,17 @@
 			name="password"
 			id="password"
 		/><br />
-		<button class="btn mt-4 w-full" type="submit">Login</button>
+		<button class="btn btn-primary mt-8 w-full" type="submit">Login</button>
 	</form>
 </div>
-<div class="flex flex-col">
-	<a class="btn" href="/register">Create an account</a>
-	<a class="btn mt-4" href="/login/github">Sign in with Github</a>
-	<a class="btn mt-4" href="/login/google">Sign in with Google</a>
+<div class="flex flex-col gap-1">
+	<a class="btn btn-ghost" href="/login/github">
+		<Icon icon="fa6-brands:github" /> Sign in with Github
+	</a>
+	<a class="btn btn-ghost" href="/login/google">
+		<Icon icon="devicon:google" /> Sign in with Google
+	</a>
+	<a class="btn btn-ghost" href="/login/google">
+		<Icon icon="devicon:facebook" /> Sign in with facebook
+	</a>
 </div>
